dc2dbaab6c88299318fc8f87a632a2518ecaaa0d
Ibrahim <ibrahimkholil550@gmail.com>
2021-01-18 13:19:38 +0600

    refactored file name

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import './index.scss';
+import "./index.scss";
 
-import Application from './components/Application';
+import Application from "./components/Application";
 
-render(<Application />, document.getElementById('root'));
+render(<Application />, document.getElementById("root"));
