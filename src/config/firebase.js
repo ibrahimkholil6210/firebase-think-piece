b7ce1e9f1244442b15be31fa23a94871c86156ea
iamibrahimkholil <Ibrahim.Kholil@teraark.com>
2021-08-01 22:16:29 +0600

    added env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwNPRVsyrER-9l3yUHykIfcujY-y0pFPw",
-  authDomain: "cloud-cafe-6ae9d.firebaseapp.com",
-  databaseURL: "https://cloud-cafe-6ae9d.firebaseio.com",
-  projectId: "cloud-cafe-6ae9d",
-  storageBucket: "cloud-cafe-6ae9d.appspot.com",
-  messagingSenderId: "953859974759",
-  appId: "1:953859974759:web:361c36dd549d17a5cc96c2",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 window.firebase = firebase;
